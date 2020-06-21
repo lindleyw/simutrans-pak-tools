2fddc812a13bc2fff3640626bcb37635c5dbe7a6
William Lindley <wlindley@wlindley.com>
2020-06-20 19:53:00 -0700

    Move image handling code into load()

--- t/2.pl
+++ t/2.pl
@@ -3,5 +3,7 @@ use Games::Simutrans::Pak;
 $p=Pak->new;
 $p->path('~/Documents/games/simutrans/simutrans-pak128.britain');
 $p->load;
+# $p->find_all_images;
+# $p->find_image_tile_sizes;
 $DB::single = 1;
 print "Bork!";
